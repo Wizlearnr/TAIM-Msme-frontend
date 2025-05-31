9c039ad5de613b9a82f56f018a5c37f9c5aa97a7
RaveenWLExt <raveen.anbarasan.ext@wizlearnr.com>
2025-06-01 02:55:06 +0530

    Add static params generation to layout and configure output for Next.js

--- app/profile/[action]/layout.tsx
+++ app/profile/[action]/layout.tsx
@@ -1,7 +1,9 @@
-"use client";
-
 import { ReactNode } from "react";
 import { ProfileActionProvider } from "./_context/CreateEditProfileContext";
+
+export async function generateStaticParams() {
+  return [{ action: "create" }, { action: "edit" }];
+}
 
 export default function Layout({ children }: { children: ReactNode }) {
   return <ProfileActionProvider>{children}</ProfileActionProvider>;
