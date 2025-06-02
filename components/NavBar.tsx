61cf3bffcef74a0565f523cfed57ef8430c56f6a
RaveenWLExt <raveen.anbarasan.ext@wizlearnr.com>
2025-06-02 19:16:00 +0530

    navbar update

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -74,19 +74,22 @@ const NavBar = () => {
       <div className="p-2 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center py-4">
           <div className="flex items-center space-x-3">
-            <div className="w-10 h-10 bg-gradient-to-br from-blue-600 to-purple-600 rounded-lg flex items-center justify-center">
-              {/* <Sparkles className="w-6 h-6 text-white" /> */}
-              <Image
-                src="/images/logo.png"
-                alt="MSME Logo"
-                width={40}
-                height={40}
-                className="rounded-lg"
-              />
-            </div>
-            <h1 className="text-xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
-              MSME Scheme Sahayak
-            </h1>
+          <div className="w-12 h-12 rounded-lg relative overflow-hidden">
+          <Image
+            src="/images/logo.png"
+            alt="MSME Logo"
+            fill
+            className="object-contain scale-[1.9]"
+          />
+          </div>
+          <div className="flex flex-col space-y-1">
+          <h1 className="text-xl font-bold bg-gradient-to-r from-blue-600 to-purple-600 bg-clip-text text-transparent">
+            MSME Scheme Sahayak
+          </h1>
+          <p className="text-sm text-gray-600 ml-3 -mt-1">
+            {'     '}మీ ప్రభుత్వ పథక మార్గదర్శక నేస్తం
+          </p>
+  </div>
           </div>
 
           <div className="relative">
